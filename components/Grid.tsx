9e57762a9213bb7d7acd36749dc4992580c61175
Kelly Wang <kellyw1806@gmail.com>
2025-01-16 04:13:44 -0500

    small fix

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -5,21 +5,23 @@ const Grid = () => {
   return (
     <section id="about">
       <BentoGrid className="w-full py-20">
-        {gridItems.map((item, i) => (
-          <BentoGridItem
-            id={item.id}
-            key={i}
-            title={item.title}
-            description={item.description}
-            // remove icon prop
-            // remove original classname condition
-            className={item.className}
-            img={item.img}
-            imgClassName={item.imgClassName}
-            titleClassName={item.titleClassName}
-            spareImg={item.spareImg}
-          />
-        ))}
+        {Array.isArray(gridItems) && gridItems.length > 0 ? (
+          gridItems.map((item) => (
+            <BentoGridItem
+              id={item.id}
+              key={item.id} // Avoid using array index as key
+              title={item.title || "Default Title"}
+              description={item.description || "No description available"}
+              className={item.className || ""}
+              img={item.img || "/images/default.png"}
+              imgClassName={item.imgClassName || ""}
+              titleClassName={item.titleClassName || ""}
+              spareImg={item.spareImg || ""}
+            />
+          ))
+        ) : (
+          <p>No grid items available.</p>
+        )}
       </BentoGrid>
     </section>
   );
